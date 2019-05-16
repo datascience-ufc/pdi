9275e2ca1e90adb9d91a6986b5d005b1e5fcf062
Manoel Vilela <manoel_vilela@engineer.com>
2019-05-16 20:02:29 -0300

    Add initial scripts

---
+++ load-cam.sh
@@ -0,0 +1,4 @@
+# source: https://www.dobitaobyte.com.br/fazer-streaming-com-camera-do-raspberry-e-controles-com-v4l2/
+# Dependências
+# sudo apt-get install build-essential v4l-utils
+sudo modprobe bcm2835-v4l2
